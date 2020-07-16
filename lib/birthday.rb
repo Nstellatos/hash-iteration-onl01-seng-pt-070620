45a8e49a9b3002866716a8d04ddb1d97b9f23623
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-16 20:01:40 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,8 +8,8 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
-def age_appropriate_birthday(new_array)
-  new_array.each do |kids_name, age|
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
     if age < 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
